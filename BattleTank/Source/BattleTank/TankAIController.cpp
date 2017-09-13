5af90dca8cec72fd3c9d031d26c2a5dc76788631
KUROBUTA\shinka.midori <shinka.midori@gmail.com>
2017-09-13 11:26:20 -0500

    Created the AI controller class

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController not possesing a tank."))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController possesing: %s"), *ControlledTank->GetName())
+	}
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController didn't find a player tank."))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found a player on: %s"), *PlayerTank->GetName())
+	}
+}
+
+ATank *ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank *ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,21 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank."))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *ControlledTank->GetName())
+	}
+	
+}
+
 ATank *ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
